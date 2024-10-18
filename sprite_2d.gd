0a08e86ed58fdf63699ed71e57965ade1c1f78c4
lmarcoreus56 <ccgomezcruz56@gmail.com>
2024-10-17 21:33:20 -0500

    final version

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -1,23 +1,35 @@
 extends Sprite2D
 
+signal health_depleted
+signal health_changed(old_value,new_value)
+
 var speed = 400
 var angular_speed = PI
-
-
-func _process(delta: float):    
-    var direction = 0 
-    var velocity = Vector2.ZERO
-    if Input.is_action_pressed("ui_left"):
-        direction = -1
-    if Input.is_action_pressed("ui_right"):
-        direction = 1
-    rotation += angular_speed * direction * delta
-
-    if Input.is_action_pressed("ui_up"):
-        velocity = Vector2.UP.rotated(rotation) * speed
-    
-    position += velocity * delta
-
+var health = 10
 
 func _init():
     print("Hello Wold")
+
+func _process(delta: float):   
+    
+    rotation += angular_speed * delta
+    var velocity = Vector2.UP.rotated(rotation) * speed
+    
+    position += velocity * delta
+
+func _on_button_pressed():
+    set_process(not is_processing())
+
+func _ready():
+    var timer = get_node("Timer")
+    timer.timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout():
+    visible = not visible
+
+func take_damage(amount):
+    var old_health = health
+    health -= amount
+    if health <= 0:
+        health_depleted.emit()
+    health_changed.emit(old_health, health)
